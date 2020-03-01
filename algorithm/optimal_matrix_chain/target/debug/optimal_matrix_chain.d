56c8a6899d1df255ee8f0cc5588464845a44c54c
Victor Deleau <victor.deleau@gmx.com>
2020-03-01 08:32:55 -0500

    cleanup + working optimal_matrix_chain

--- algorithm/optimal_matrix_chain/target/debug/optimal_matrix_chain.d
+++
@@ -1,1 +0,0 @@
-/home/victor/Bureau/code/rust-plinth/algorithm/optimal_matrix_chain/target/debug/optimal_matrix_chain: /home/victor/Bureau/code/rust-plinth/algorithm/optimal_matrix_chain/src/main.rs
