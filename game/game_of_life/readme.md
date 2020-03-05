fddaab39b26363a8104eb7446c04701f9e84b4c4
Victor Deleau <victor.deleau@gmx.com>
2020-03-05 18:06:22 -0500

    array to vec + refactor + update readme of game of life

--- game/game_of_life/readme.md
+++ game/game_of_life/readme.md
@@ -1,3 +1,7 @@
 # game of life
 
 A CLI implementation of the game of life in a 32x32 grid. It is a cellular automaton created by John Horton Conway in 1970. Interestingly, the game of life is Turing complete.
+
+<p align="center">
+  <img src="./screenshot.png">
+</p>
--- game/game_of_life/src/main.rs
+++ game/game_of_life/src/main.rs
@@ -1,134 +1,193 @@
-// for testing stuff
-
 use rand::Rng;
 use std::{thread, time};
+use std::vec::Vec;
 
-use ansi_escapes::*;
+use ansi_escapes::*; // specific characters
 
-const ESC: char = 27u8 as char;
-const BACKSPACE: char = 8u8 as char;
+const GRID_SIZE: (usize, usize) = (32, 32);
+const PROBABILITY: f32 = 0.4;
+const FPS: f32 = 10.0;
 
-// print the grid in the terminal
-fn print_grid(){
 
-    ;
+fn print_grid(grid_size: (usize, usize), grid: &Vec<Vec<char>>){
+    /*
+    print the grid in the terminal
+    input
+        grid_size: (usize, usize)
+        grid: &Vec<Vec<char>>
+    output
+        None
+    */
 
+    print!("    ");
+    for _ in  0..(grid_size.0+2)*2 {print!("\u{2588}");}
+    println!("");
+
+    for i in  0..grid_size.1 {
+        print!("    \u{2588}\u{2588}");
+        for j in  0..grid_size.0 {
+            print!("{}{}", grid[i][j], grid[i][j]);
+        }
+        println!("\u{2588}\u{2588}");
+    }
+    print!("    ");
+    for _ in  0..(grid_size.0+2)*2 {print!("\u{2588}");}
+    println!("\n");
 }
 
-fn erase_grid(){
 
-    ;
+fn update_grid(grid_size: (usize, usize), current_grid: &Vec<Vec<char>>, new_grid: &mut Vec<Vec<char>>) {
+    /*
+    update grid state
+    input
+        grid_size: (usize, usize)
+        current_grid: &Vec<Vec<char>>
+        new_grid: &mut Vec<Vec<char>>
+    output
+        None
+    */
+
+    for i in  0..grid_size.1 {
+        for j in  0..grid_size.0 {
+
+            let x = nb_alive_neighbor(&current_grid, (i, j));
+
+            if current_grid[i][j] == '\u{2588}' { // is alive
+                if x < 2 || x > 3 { // cell die
+                    new_grid[i][j] = ' ';
+                }
+            } else { // cell is dead
+                if x == 3 { // cell becomes alive
+                    new_grid[i][j] = '\u{2588}';
+                }
+            }
+        }
+    }
 }
 
 
-// check the number of alive neighbor
-fn nb_alive_neighbor(grid: &[[char; 32]; 32], cell: (usize, usize)) -> u8 {
+fn nb_alive_neighbor(grid: &Vec<Vec<char>>, cell: (usize, usize)) -> u8 {
+    /*
+    Takes a cell index as input and return its number of alive neighbor.
+    input
+        grid: &[[char; 32]; 32]
+        cell: (usize, usize)
+    output
+        u8
+    */
 
     let mut nb_alive_neighbor: u8 = 0;
 
-    if cell.0 != 0 { // check top
+    // check top
+    if cell.0 != 0 { 
 
         if grid[cell.0-1][cell.1] == '\u{2588}' { nb_alive_neighbor += 1; }
 
-        if cell.1 != grid[0].len()-1 { // check top right
-            if grid[cell.0-1][cell.1+1] == '\u{2588}' { nb_alive_neighbor += 1; }
+        // check top right
+        if cell.1 != grid[0].len()-1 { 
+            if grid[cell.0-1][cell.1+1] == '\u{2588}' {
+                nb_alive_neighbor += 1;
+            }
         }
 
-        if cell.1 != 0 { // check top left
-            if grid[cell.0-1][cell.1-1] == '\u{2588}' { nb_alive_neighbor += 1; }
+        // check top left
+        if cell.1 != 0 { 
+            if grid[cell.0-1][cell.1-1] == '\u{2588}' {
+                nb_alive_neighbor += 1;
+            }
         }
     }
     
-    if cell.0 != grid.len()-1 { // check bottom
+    // check bottom
+    if cell.0 != grid.len()-1 { 
 
-        if grid[cell.0+1][cell.1] == '\u{2588}' { nb_alive_neighbor += 1; }
+        if grid[cell.0+1][cell.1] == '\u{2588}' {
+            nb_alive_neighbor += 1;
+        }
 
-        if cell.1 != 0 { // check bottom left
-            if grid[cell.0+1][cell.1-1] == '\u{2588}' { nb_alive_neighbor += 1; }
+        // check bottom left
+        if cell.1 != 0 { 
+            if grid[cell.0+1][cell.1-1] == '\u{2588}' {
+                nb_alive_neighbor += 1;
+            }
         }
         
-        if cell.1 != grid[0].len()-1 { // check bottom right
-            if grid[cell.0+1][cell.1+1] == '\u{2588}' { nb_alive_neighbor += 1; }
+        // check bottom right
+        if cell.1 != grid[0].len()-1 { 
+            if grid[cell.0+1][cell.1+1] == '\u{2588}' {
+                nb_alive_neighbor += 1;
+            }
         }
     }    
 
-    if cell.1 != 0 { // check left
-        if grid[cell.0][cell.1-1] == '\u{2588}' { nb_alive_neighbor += 1; }
+    // check left
+    if cell.1 != 0 { 
+        if grid[cell.0][cell.1-1] == '\u{2588}' {
+            nb_alive_neighbor += 1;
+        }
     }
     
-    if cell.1 != grid[0].len()-1 { // check right
-        if grid[cell.0][cell.1+1] == '\u{2588}' { nb_alive_neighbor += 1; } 
+    // check right
+    if cell.1 != grid[0].len()-1 { 
+        if grid[cell.0][cell.1+1] == '\u{2588}' {
+            nb_alive_neighbor += 1;
+        } 
     }
 
     return nb_alive_neighbor;
 }
 
 
-fn main(){
+fn initialize_grid(probability: f32) -> Vec<Vec<char>> {
+    /*
+    Initialize a new grid of cells according to probability.
+    input
+        probability: f32
+    output
+        Vec<Vec<i64>>
+    */
 
-    println!("\n\n                        Welcome to the game of life !");
-
-    const GRID_SIZE: (usize, usize) = (32, 32);
-    const LIFE_PROBABILITY: f32 = 0.4;
-    const FPS: f32 = 2.0;
-
-    let mut current_grid = [[' '; GRID_SIZE.0]; GRID_SIZE.1];
-    let mut new_grid = [[' '; GRID_SIZE.0]; GRID_SIZE.1];
-
-    // initialize random number generator
     let mut rng = rand::thread_rng();
 
-    // initialize grid
-    for i in  0..GRID_SIZE.1 {
-        for j in  0..GRID_SIZE.0 {
-            let draw: f32 = rng.gen();
-            if draw < LIFE_PROBABILITY {
-                // cell is alive
-                current_grid[i][j] = '\u{2588}'; 
-            } else {
-                // cell is dead
-                current_grid[i][j] = ' ';
-            }
-        }
-    }
+    (0..GRID_SIZE.0).map(|_| {
+        (0..GRID_SIZE.1).map(|_| {
 
-    ///////////////////////////////////////////////////////////////////
-    // main game lopp /////////////////////////////////////////////////
+            // cell is alive
+            if rng.gen::<f32>() < probability { '\u{2588}' } 
+
+            // cell is dead
+            else { ' ' } 
+            
+        }).collect()
+    }).collect()
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// main 
+////////////////////////////////////////////////////////////////////////////////
+
+fn main(){
+
+    println!("\n\n                        Welcome to the game of life !\n");
     
-    //let delay = time::Duration::from_millis((1000.0/FPS) as u64);
-    let delay = time::Duration::from_millis(100);
+    let mut current_grid = initialize_grid(PROBABILITY);
 
-    while true {
+    let mut new_grid: Vec<Vec<char>>=vec![vec![' '; GRID_SIZE.1]; GRID_SIZE.0];
+    
+    let delay = time::Duration::from_millis((1.0/FPS * 1000.0) as u64);
+
+    loop {
 
         thread::sleep(delay);
-        print!("    ");
-        for i in  0..(GRID_SIZE.0+2)*2 {print!("\u{2588}");}
-        println!("");
 
-        for i in  0..GRID_SIZE.1 {
-            print!("    \u{2588}\u{2588}");
-            for j in  0..GRID_SIZE.0 {
-                let x = nb_alive_neighbor(&current_grid, (i, j));
-                if current_grid[i][j] == '\u{2588}' { // is alive
-                    if x < 2 || x > 3 { // cell die
-                        new_grid[i][j] = ' ';
-                    }
-                } else { // cell is dead
-                    if x == 3 { // cell becomes alive
-                        new_grid[i][j] = '\u{2588}';
-                    }
-                }
-                print!("{}", current_grid[i][j]);
-                print!("{}", current_grid[i][j]);
-            }
-            println!("\u{2588}\u{2588}");
-        }
-        print!("    ");
-        for i in  0..(GRID_SIZE.0+2)*2 {print!("\u{2588}");}
-        println!("\n");
+        print_grid(GRID_SIZE, &current_grid);
 
-        current_grid = new_grid;
+        update_grid(GRID_SIZE, &current_grid, &mut new_grid);
+        
+        current_grid = new_grid.to_owned();
 
-        for i in 0..GRID_SIZE.0+3 { print!("{}{}", CursorUp(1), EraseLine); }
+        // erase terminal
+        for _ in 0..GRID_SIZE.0+3 { print!("{}{}", CursorUp(1), EraseLine); }
     }
 }
